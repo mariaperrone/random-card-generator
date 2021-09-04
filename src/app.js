b0bcb2e870be0469078f7e203eae74ba87af16d6
mariaperrone <perronemar148@gmail.com>
2021-09-04 01:28:55 +0000

    en progreso

--- src/app.js
+++ src/app.js
@@ -5,7 +5,72 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const getRandom = maxCant => {
+  return Math.floor(Math.random() * maxCant);
+};
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let listan = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomn = getRandom(listan.length);
+  let numero = document.querySelector(".numero");
+  numero.innerHTML = listan[randomn];
+
+  let listap = ["♦", "♥", "♠", "♣"];
+  let randomp = getRandom(listap.length);
+  let palo = document.querySelector(".palo");
+  let palorotado = document.querySelector(".palorotado");
+  palo.innerHTML = listap[randomp];
+  palorotado.innerHTML = listap[randomp];
+  if (randomp < 2) {
+    palo.style.color = "red";
+    palorotado.style.color = "red";
+  }
+};
+
+let boton = document.querySelector("#hello");
+boton.onclick = function() {
+  let listan = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomn = getRandom(listan.length);
+  let numero = document.querySelector(".numero");
+  numero.innerHTML = listan[randomn];
+
+  let listap = ["♦", "♥", "♠", "♣"];
+  let randomp = getRandom(listap.length);
+  let palo = document.querySelector(".palo");
+  let palorotado = document.querySelector(".palorotado");
+  palo.innerHTML = listap[randomp];
+  palorotado.innerHTML = listap[randomp];
+  if (randomp < 2) {
+    palo.style.color = "red";
+    palorotado.style.color = "red";
+  }
 };
