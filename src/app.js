8d6382353d581203729dcf0e0d2f37d34ea4f7b9
mariaperrone <perronemar148@gmail.com>
2021-09-04 02:14:21 +0000

    terminado con boton extra

--- src/app.js
+++ src/app.js
@@ -8,28 +8,16 @@ import "./assets/img/4geeks.ico";
 const getRandom = maxCant => {
   return Math.floor(Math.random() * maxCant);
 };
-window.onload = function() {
-  let listan = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
 
+let listan = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let listap = ["♦", "♥", "♠", "♣"];
+
+function mostrarEvento() {
   let randomn = getRandom(listan.length);
   let numero = document.querySelector(".numero");
   numero.innerHTML = listan[randomn];
 
-  let listap = ["♦", "♥", "♠", "♣"];
   let randomp = getRandom(listap.length);
   let palo = document.querySelector(".palo");
   let palorotado = document.querySelector(".palorotado");
@@ -38,39 +26,17 @@ window.onload = function() {
   if (randomp < 2) {
     palo.style.color = "red";
     palorotado.style.color = "red";
+  } else {
+    palo.style.color = "black";
+    palorotado.style.color = "black";
   }
+}
+
+window.onload = function() {
+  mostrarEvento();
 };
 
 let boton = document.querySelector("#hello");
 boton.onclick = function() {
-  let listan = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-
-  let randomn = getRandom(listan.length);
-  let numero = document.querySelector(".numero");
-  numero.innerHTML = listan[randomn];
-
-  let listap = ["♦", "♥", "♠", "♣"];
-  let randomp = getRandom(listap.length);
-  let palo = document.querySelector(".palo");
-  let palorotado = document.querySelector(".palorotado");
-  palo.innerHTML = listap[randomp];
-  palorotado.innerHTML = listap[randomp];
-  if (randomp < 2) {
-    palo.style.color = "red";
-    palorotado.style.color = "red";
-  }
+  mostrarEvento();
 };
